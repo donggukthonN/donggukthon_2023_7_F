7ae6c168dc974eed79f496f92e67d4f97ac7535a
sayyyho <323psh@naver.com>
2023-12-19 01:10:20 +0900

    Feat: 로딩화면 구현

--- src/pages/Delete/Delete.jsx
+++ src/pages/Delete/Delete.jsx
@@ -1,39 +1,53 @@
+import React, { useState, useEffect } from "react";
 import { LocationInput } from "../../components/index";
+import LoadingPage from "../Loading/LoadingPage";
+import { useReverseGeocoding } from "../../hooks/useReverseGeocoding";
+import styles from "../../pages/Delete/Delete.module.css";
+
 import {
   HeartButton,
-  PhotoUpload,
   ShareButton,
-  PhotoDisplay
+  PhotoDisplay,
 } from "../../components/Button/Button";
 import { PasswordCheck } from "../../components/Input/Input";
 import { useGeoLocation } from "../../hooks/useGeoLocation";
 
-import styles from "../../pages/Delete/Delete.module.css";
-
 const Delete = () => {
+  const [lat, setLat] = useState();
+  const [lng, setLng] = useState();
+  const [address, setAddress] = useState();
   const { loc } = useGeoLocation();
+  useEffect(() => {
+    if (loc) {
+      setLat(loc.latitude);
+      setLng(loc.longitude);
+    }
+  }, [loc]);
+  const data = useReverseGeocoding(lat, lng);
+
+  useEffect(() => {
+    setAddress(data);
+  }, [data]);
 
   return (
     <div className={styles.Deletepage}>
-      <div className={styles.Deletecomponen}>
-        <div className={styles.LocationInput}>
-          <LocationInput
-            location={
-              loc
-                ? `lat : ${loc.latitude} lng : ${loc.longitude}`
-                : "현재 위치 로딩중"
-            }
-          />
+      {address ? (
+        <div className={styles.Deletecomponen}>
+          <div className={styles.LocationInput}>
+            <LocationInput location={address} />
+          </div>
+          <div className={styles.PhotoDisplay}>
+            <PhotoDisplay />
+          </div>
+          <div className={styles.DeleteButtons}>
+            <HeartButton />
+            <ShareButton />
+            <PasswordCheck />
+          </div>
         </div>
-        <div className={styles.PhotoDisplay}>
-          <PhotoDisplay />
-        </div>
-        <div className={styles.DeleteButtons}>
-          <HeartButton />
-          <ShareButton />
-          <PasswordCheck />
-        </div>
-      </div>
+      ) : (
+        <LoadingPage title={"현재 위치를 파악중입니다."} />
+      )}
     </div>
   );
 };
--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,22 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { useGeoLocation } from "../../hooks/useGeoLocation";
+import { useReverseGeocoding } from "../../hooks/useReverseGeocoding";
 import { LocationInput } from "../../components/index";
 import { PhotoUpload, UploadButton } from "../../components/Button/Button";
 import { Name, PasswordInput } from "../../components/Input/Input";
-
+import LoadingPage from "../Loading/LoadingPage";
 import styles from "../../pages/Upload/Upload.module.css";
 
 const Upload = () => {
-    return (
-        <div className={styles.Uploadback}>
+  const [lat, setLat] = useState();
+  const [lng, setLng] = useState();
+  const [address, setAddress] = useState();
+  const { loc } = useGeoLocation();
+  useEffect(() => {
+    if (loc) {
+      setLat(loc.latitude);
+      setLng(loc.longitude);
+    }
+  }, [loc]);
+  const data = useReverseGeocoding(lat, lng);
+
+  useEffect(() => {
+    setAddress(data);
+  }, [data]);
+
+  return (
+    <div className={styles.Uploadback}>
+      {address ? (
         <div className={styles.Uploadpage}>
-            <div>
-                <div className={styles.PhotoUpload}><LocationInput /></div>
-                <div className={styles.PhotoUpload}><PhotoUpload /></div>
+          <div>
+            <div className={styles.PhotoUpload}>
+              <LocationInput location={address} />
             </div>
-            <div className={styles.UploadInput}> <Name /> <PasswordInput /> </div>
-            <div className={styles.UploadButton}><UploadButton /></div>
+            <div className={styles.PhotoUpload}>
+              <PhotoUpload />
+            </div>
+          </div>
+          <div className={styles.UploadInput}>
+            {" "}
+            <Name /> <PasswordInput />{" "}
+          </div>
+          <div className={styles.UploadButton}>
+            <UploadButton />
+          </div>
         </div>
-        </div>
-    );
-}
+      ) : (
+        <LoadingPage title={"현재 위치를 파악중입니다."} />
+      )}
+    </div>
+  );
+};
 
 export default Upload;
