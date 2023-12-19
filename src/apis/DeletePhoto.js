61ecdb7c401a66d274ce777cf71cbbb8fc8c6861
sujinee01 <kmosujin@gmail.com>
2023-12-20 07:45:33 +0900

    Designed: Photoupload 페이지 수정

--- src/apis/DeletePhoto.js
+++ src/apis/DeletePhoto.js
@@ -2,17 +2,16 @@ import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-const DeletePhoto = async (photo_id, photoInfo) => {
+const DeletePhoto = async () => {
   try {
-    const response = await axios.post(
-      `${API_URL}/photo/delete/${photo_id}`,
-      photoInfo,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const password = {
+      password: "1234",
+    };
+    const response = await axios.post(`${API_URL}/photo/delete/7`, password, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
     return response;
   } catch (error) {
