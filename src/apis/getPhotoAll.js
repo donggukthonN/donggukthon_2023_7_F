e94175a00e34d49c5f479620b4679b3098224d67
sujinee01 <kmosujin@gmail.com>
2023-12-20 00:35:26 +0900

    Your commit message here

--- src/apis/getPhotoAll.js
+++ src/apis/getPhotoAll.js
@@ -1,20 +1,5 @@
 import axios from "axios";
 
-<<<<<<< HEAD
-const API_URL = "https://api.snowmanvillage.site";
-
-const getPhotoAll = async (orderBy) => {
-  try {
-    const res = await axios.get(
-      (`${API_URL}/photo/list`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        params: { orderBy },
-      })
-    );
-=======
 const API_URL = process.env.REACT_APP_API_URL;
 const getPhotoAll = async (orderBy) => {
   try {
@@ -24,7 +9,6 @@ const getPhotoAll = async (orderBy) => {
       },
       params: { orderBy },
     });
->>>>>>> ee53f53fea4cb1ea3341def9534153a380458592
     console.log(res);
   } catch (err) {
     console.error(err);
