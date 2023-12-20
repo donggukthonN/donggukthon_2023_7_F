3279fb3b5d575e62025795c2e4a129c28d0dd2fc
sujinee01 <kmosujin@gmail.com>
2023-12-20 11:26:16 +0900

    Designed:

--- src/apis/DeletePhoto.js
+++ src/apis/DeletePhoto.js
@@ -4,15 +4,11 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 const DeletePhoto = async (password) => {
   try {
-    const response = await axios.put(
-      `${API_URL}/photo/delete/${photo_id}`,
-      photoInfo,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const response = await axios.put(`${API_URL}/photo/delete`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     return response;
   } catch (error) {
     console.error(error);
