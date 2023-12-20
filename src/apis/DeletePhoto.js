3d2fb88294c43c1158334433a8fc58b374f120bd
sujinee01 <kmosujin@gmail.com>
2023-12-20 10:41:21 +0900

    Committing local changes before pulling upstream changes

--- src/apis/DeletePhoto.js
+++ src/apis/DeletePhoto.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-const DeletePhoto = async () => {
+const DeletePhoto = async (password) => {
   try {
     const password = {
-      password: "1234",
+      password: "password",
     };
-    const response = await axios.post(`${API_URL}/photo/delete`, password, {
+    const response = await axios.post(`${API_URL}/photo/delete/${photo_id}`, password, {
       headers: {
         "Content-Type": "application/json",
       },
