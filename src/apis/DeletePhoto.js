ca1f374361deb5e182238f3b58ddf9dc588a5f3b
sayyyho <323psh@naver.com>
2023-12-20 10:53:35 +0900

    Fix: 홈 화면 깨지는 현상 해결

--- src/apis/DeletePhoto.js
+++ src/apis/DeletePhoto.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-const DeletePhoto = async () => {
+const DeletePhoto = async (photo_id, photoInfo) => {
   try {
     const response = await axios.put(
       `${API_URL}/photo/delete/${photo_id}`,
